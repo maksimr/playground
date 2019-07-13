7c3ac7e3314c13c3518a19172c30d4838eb26712
Maksim Ryzhikov <rv.maksim@gmail.com>
2019-07-13 16:54:24 +0300

    refactor: cleanup node code

--- src/Node.js
+++ src/Node.js
@@ -1,30 +1,12 @@
 export class Node {
-  static OR(leftExpressionNode, rightExpressionNode) {
-    return new Node(Node.OR, [leftExpressionNode, rightExpressionNode]);
-  }
-
-  static AND(leftExpressionNode, rightExpressionNode) {
-    return new Node(Node.AND, [leftExpressionNode, rightExpressionNode]);
-  }
-
-  static NOT(expressionNode) {
-    return new Node(Node.NOT, expressionNode);
-  }
-
-  static Field(fieldNameNode, fieldValueNode) {
-    return new Node(Node.Field, [fieldNameNode, fieldValueNode]);
-  }
-
-  static FieldName(name) {
-    return new Node(Node.FieldName, name);
-  }
-
-  static FieldValue(value) {
-    return new Node(Node.FieldValue, value);
-  }
-
   constructor(type, value) {
     this.type = type;
     this.value = value;
   }
+  static OR(xExpr, yExpr) { return new Node(Node.OR, [xExpr, yExpr]); }
+  static AND(xExpr, yExpr) { return new Node(Node.AND, [xExpr, yExpr]); }
+  static NOT(xExpr) { return new Node(Node.NOT, xExpr); }
+  static Field(nameExpr, valueExpr) { return new Node(Node.Field, [nameExpr, valueExpr]); }
+  static FieldName(name) { return new Node(Node.FieldName, name); }
+  static FieldValue(value) { return new Node(Node.FieldValue, value); }
 }
